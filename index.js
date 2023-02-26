bca8aa2c86d969b46dfc7dffb9875d8ad27043fc
Devanshu Saxena <86067292+devanshu0605@users.noreply.github.com>
2023-02-26 23:07:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,4 +12,20 @@ hamburger.addEventListener('click', ()=>{
     //Hamburger Animation
     hamburger.classList.toggle("toggle");
 });
+function addcl() {
+    let parent = this.parentNode.parentNode;
+    parent.classList.add("focus");
+}
 
+function remcl() {
+    let parent = this.parentNode.parentNode;
+    if (this.value == "") {
+        parent.classList.remove("focus");
+    }
+}
+
+
+inputs.forEach(input => {
+    input.addEventListener("focus", addcl);
+    input.addEventListener("blur", remcl);
+});
